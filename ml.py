6a849daaf586f7db16ee69a658707f81e1203e2b
Chanukya <97583921+Chanukya113118@users.noreply.github.com>
2022-04-22 20:38:34 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import streamlit as st
+import viz
+import understand
+import mlalgo
+class start:
+    def fileupload(self):
+        self.file=st.file_uploader('Upload file',type=['CSV','XLSX'])
+        if self.file is not None:
+            try:
+                self.data=pd.read_csv(self.file)
+                return self.data
+            except:
+                try:
+                    self.data=pd.read_excel(self.file)
+                    return self.data
+                except:
+                    st.warning('Supported CSV, EXCEL files only')
+    def opt(self):
+        self.option=st.sidebar.selectbox('What to do',['','Visualize','Analyze','Machine Learning'])
+        return self.option
+if __name__=='__main__':
+    obj=start()
+    data=obj.fileupload()
+    to_do=obj.opt()
+    if data is not None:
+        if to_do=='Analyze':
+            understand.analyse_class.analyse_fun(data)
+        if to_do=='Visualize':
+            viz.viz_class.visualization(data)
+        if to_do=='Machine Learning':
+            mlalgo.ml_class.algo(data)
---
+++ understand.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+class analyse_class:
+    def analyse_fun(data):
+        data=data
+        col1,col2=st.columns(2)
+        if col1.button('Describe'):
+            st.write(data.describe())
+        if col2.button('Missing value Information'):
+            st.write(data.isnull().sum())
+            
+            
